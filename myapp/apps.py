8e623c2359a010d960c7118543dd05e266be586d
FarhanSheikh-01 <sheikhfarhan001.sf@gmail.com>
2025-01-20 11:54:08 +0530

    Django Project Added

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,27 @@
 from django.apps import AppConfig
+from joblib import load
+import os
 
-
-class MyappConfig(AppConfig):
+class MYAPPConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "myapp"
+    label = "MYAPP"
+
+    def ready(self):
+        # Define the path to the 'SaveModels' directory
+        save_models_dir = os.path.join('D:\\My_Docs\\sentiment_analysis\\myproject', 'SaveModels')
+
+        # Construct the full paths to the model and vector files
+        model_path = os.path.join(save_models_dir, 'model.joblib')
+        vector_path = os.path.join(save_models_dir, 'vector.joblib')
+
+        # Load the model and vector
+        if os.path.exists(model_path):
+            self.model = load(model_path)
+        else:
+            raise FileNotFoundError(f"Model file not found at {model_path}")
+
+        if os.path.exists(vector_path):
+            self.vector = load(vector_path)
+        else:
+            raise FileNotFoundError(f"Vector file not found at {vector_path}")
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,15 @@
+# Base_App/views.py
 from django.shortcuts import render
+from django.apps import apps
 
-# Create your views here.
+def msg(request):
+    if request.method == "POST":
+        text = request.POST.get("text", "")
+        if text:
+            app_config = apps.get_app_config('MYAPP')  
+            transformed_text = app_config.vector.transform([text])
+            prediction = app_config.model.predict(transformed_text)
+            return render(request, 'index.html', {"prediction": prediction[0]})
+        else:
+            return render(request, 'index.html', {"error": "No text provided."})
+    return render(request, 'index.html', {"error": "Invalid request method."})
